7078b82d5e47ac770c3eb2fd3520104344934573
Yagnik Kantelia <yagnik@icoderzsolutions.com>
2021-04-14 10:46:37 +0530

    Dashboard Screen

---
+++ app/src/main/java/com/jaylax/wiredshack/home/HomeFragment.java
@@ -0,0 +1,31 @@
+package com.jaylax.wiredshack.home;
+
+import android.os.Bundle;
+
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jaylax.wiredshack.R;
+import com.jaylax.wiredshack.databinding.FragmentHomeBinding;
+
+public class HomeFragment extends Fragment {
+    FragmentHomeBinding mBinding;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false);
+        return mBinding.getRoot();
+    }
+}
